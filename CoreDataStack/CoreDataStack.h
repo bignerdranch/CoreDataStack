13beaf0bda0cb1b6f7a77ff0f2db75ca74a5fcee
Robert Edwards <robert@bignerdranch.com>
2015-04-24 16:40:36 -0400

    Import swift header in base framework header.

--- CoreDataStack/CoreDataStack.h
+++ CoreDataStack/CoreDataStack.h
@@ -14,6 +14,4 @@ FOUNDATION_EXPORT double CoreDataStackVersionNumber;
 //! Project version string for CoreDataStack.
 FOUNDATION_EXPORT const unsigned char CoreDataStackVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStack/PublicHeader.h>
-
-
+#import "CoreDataStack-Swift.h"
