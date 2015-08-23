8c2553df39bf3edd0aaffa92d0d20f254d709a34
Brian Hardy <brian@bignerdranch.com>
2015-08-23 16:00:23 -0400

    Fixes compiler error in Example project.

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        try! CoreDataStack.constructStack(withModelName: "TestModel") { result in
+        CoreDataStack.constructSQLiteStack(withModelName: "TestModel") { result in
             switch result {
             case .Success(let stack):
                 self.coreDataStack = stack
