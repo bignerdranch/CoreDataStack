0fb58f86d92a2b03c1c033853eb92043806f0b8f
travis-ci <travis@travis-ci.org>
2015-11-05 10:42:55 -0500

    Add script to automatically generate docs and push to gh-pages branch.

---
+++ Resources/scripts/publish_docs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo -e "Generating docs \n"
+
+    echo -e "Cloning gh-pages branch \n"
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/bignerdranch/CoreDataStack gh-pages > /dev/null
+
+    echo -e "Installing Jazzy \n"
+    gem install jazzy
+
+    echo -e "Generating Jazzy output \n"
+    jazzy --swift-version 2.1 --source-directory ./ --output ./gh-pages --podspec ./BNRCoreDataStack.podspec
+
+    echo -e "Moving into gh-pages clone"
+    pushd gh-pages
+
+    echo -e "Adding new docs \n"
+    git add .
+    git commit -m "Refresh docs from successful travis build $TRAVIS_BUILD_NUMBER"
+    git push -fq origin gh-pages > /dev/null
+
+    echo -e "Moving out of gh-pages clone and cleaing up"
+    popd
+    rm -rf gh-pages
+
+    echo -e "Published latest docs.\n"
+fi
