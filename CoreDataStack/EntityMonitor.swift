ffa3b9f9c7cd1c226abadc7310e577e5158a21ed
Robert Edwards <robert@bignerdranch.com>
2015-11-19 12:03:09 -0500

    Add a class for monitoring Inserts, Deletes, and Updates to a specific entity.

---
+++ CoreDataStack/EntityMonitor.swift
@@ -0,0 +1,154 @@
+//
+//  EntityMonitor.swift
+//  CoreDataStack
+//
+//  Created by Robert Edwards on 11/18/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import CoreData
+
+/// The freqency of notification dispatch from the EntityMonitor
+public enum FireFrequency {
+    /// Notifications will be sent upon MOC being changed
+    case OnChange
+    /// Notifications will be sent upon MOC being saved
+    case OnSave
+}
+
+/**
+ Protocol for delegate callbacks of NSManagedObject entity change events.
+ */
+public protocol EntityMonitorDelegate: class { // : class for weak capture
+    typealias T: NSManagedObject, CoreDataModelable
+
+    func entityMonitorObservedInserts(monitor: EntityMonitor<T>, entities: Set<T>)
+    func entityMonitorObservedDeletions(monitor: EntityMonitor<T>, entities: Set<T>)
+    func entityMonitorObservedModifications(monitor: EntityMonitor<T>, entities: Set<T>)
+}
+
+/**
+ Class for monitoring changes within a given NSManagedObjectContext
+    to a specific Core Data Entity with optional filtering via an NSPredicate.
+ */
+public class EntityMonitor<T: NSManagedObject where T: CoreDataModelable> {
+
+    // MARK: - Public Properties
+
+    /**
+     Function for setting the EntityMonitorDelegate that will receive callback events.
+
+     - parameter U: Your delegate must implement the methods in EntityMonitorDelegate with the matching CoreDataModelable type being monitored.
+     */
+    public func setDelegate<U: EntityMonitorDelegate where U.T == T>(delegate: U) {
+        self.delegate = InternalEntityMonitorDelegate(delegate)
+    }
+
+    // MARK: - Private Properties
+
+    private var delegate: InternalEntityMonitorDelegate<T>?
+
+    private typealias EntitySet = Set<T>
+
+    private let context: NSManagedObjectContext
+    private let frequency: FireFrequency
+    private let entityPredicate: NSPredicate
+    private let filterPredicate: NSPredicate?
+    private var combinedPredicate: NSPredicate {
+        if let filterPredicate = filterPredicate {
+            return NSCompoundPredicate(andPredicateWithSubpredicates:
+                [entityPredicate, filterPredicate])
+        } else {
+            return entityPredicate
+        }
+    }
+
+    // MARK: - Lifecycle
+
+    /**
+    Initializer to create an EntityMonitor to monitor changes to a specific Core Data Entity.
+
+    This initializer is failable in the event your Entity is not within the supplied NSManagedObjectContext.
+
+    - parameter context: NSManagedObjectContext the context you want to monitor changes within.
+    - parameter frequency: FireFrequency How frequently you wish to receive callbacks of changes. Default value is OnSave.
+    - parameter filterPredicate: An optional filtering predicate to be applied to entities being monitored.
+    */
+    public init?(context: NSManagedObjectContext, frequency: FireFrequency = .OnSave, filterPredicate: NSPredicate? = nil) {
+        self.context = context
+        self.frequency = frequency
+        self.filterPredicate = filterPredicate
+        guard let entity = NSEntityDescription.entityForName(T.entityName, inManagedObjectContext: context) else {
+            entityPredicate = NSPredicate()
+            return nil
+        }
+
+        entityPredicate = NSPredicate(format: "entity == %@", entity)
+        setupObservers()
+    }
+
+    // MARK: - Private
+
+    private func setupObservers() {
+        let notificationName: String
+        switch frequency {
+        case .OnChange:
+            notificationName = NSManagedObjectContextObjectsDidChangeNotification
+        case .OnSave:
+            notificationName = NSManagedObjectContextDidSaveNotification
+        }
+
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: ChangeObserverSelectorName,
+            name: notificationName,
+            object: context)
+    }
+
+    // MARK: - Notifications
+
+    @objc private func evaluateChangeNotification(notification: NSNotification) {
+        guard let changeSet = notification.userInfo else {
+            return
+        }
+
+        context.performBlockAndWait() {
+            if let inserted = changeSet[NSInsertedObjectsKey],
+                filtered = inserted.filteredSetUsingPredicate(self.combinedPredicate)
+                    as? EntitySet where filtered.count > 0 {
+                        self.delegate?.objectsInserted(self, filtered)
+            }
+
+            if let deleted = changeSet[NSDeletedObjectsKey],
+                filtered = deleted.filteredSetUsingPredicate(self.combinedPredicate)
+                    as? EntitySet where filtered.count > 0 {
+                       self.delegate?.objectsRemoved(self, filtered)
+            }
+
+            if let updated = changeSet[NSUpdatedObjectsKey],
+                filtered = updated.filteredSetUsingPredicate(self.combinedPredicate)
+                    as? EntitySet where filtered.count > 0 {
+                        self.delegate?.objectsUpdated(self, filtered)
+            }
+        }
+    }
+}
+
+private let ChangeObserverSelectorName: Selector = "evaluateChangeNotification:"
+
+private struct InternalEntityMonitorDelegate<T: NSManagedObject where T: CoreDataModelable> {
+    let objectsInserted: (EntityMonitor<T>, Set<T>) -> Void
+    let objectsRemoved: (EntityMonitor<T>, Set<T>) -> Void
+    let objectsUpdated: (EntityMonitor<T>, Set<T>) -> Void
+
+    init<U: EntityMonitorDelegate where U.T == T>(_ delegate: U) {
+        objectsInserted = { [weak delegate] in
+            delegate?.entityMonitorObservedInserts($0, entities: $1)
+        }
+        objectsRemoved = { [weak delegate] in
+            delegate?.entityMonitorObservedDeletions($0, entities: $1)
+        }
+        objectsUpdated = { [weak delegate] in
+            delegate?.entityMonitorObservedModifications($0, entities: $1)
+        }
+    }
+}
---
+++ CoreDataStackTests/EntityMonitorTests.swift
@@ -0,0 +1,172 @@
+//
+//  EntityMonitorTests.swift
+//  CoreDataStack
+//
+//  Created by Robert Edwards on 11/18/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import XCTest
+import CoreData
+import CoreDataStack
+
+class EntityMonitorTests: TempDirectoryTestCase, EntityMonitorDelegate {
+
+    var stack: CoreDataStack!
+    var monitor: EntityMonitor<Author>!
+    var filteredMonitor: EntityMonitor<Author>!
+
+    var insertExpectation: XCTestExpectation!
+    var deleteExpectation: XCTestExpectation!
+    var updateExpectation: XCTestExpectation!
+
+    override func setUp() {
+        super.setUp()
+
+        let setupEx = expectationWithDescription("Setup")
+        let bundle = NSBundle(forClass: CoreDataStackTests.self)
+
+        CoreDataStack.constructSQLiteStack(withModelName: "TestModel", inBundle: bundle, withStoreURL: tempStoreURL) { result in
+            switch result {
+            case .Success(let stack):
+                self.stack = stack
+            case .Failure(let error):
+                print(error)
+                XCTFail()
+            }
+            setupEx.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(10, handler: nil)
+
+        // Insert and save a new item so we can test updates
+        let moc = stack.mainQueueContext
+        let fr = NSFetchRequest(entityName: Author.entityName)
+        let results = try! moc.executeFetchRequest(fr)
+        if results.count < 1 {
+            let _ = Author.newInContext(stack.mainQueueContext)
+            try! moc.saveContextAndWait()
+        }
+    }
+
+    // MARK: - Tests
+
+    func testOnSaveNotifications() {
+        // Setup monitor
+        let moc = stack.mainQueueContext
+        monitor = EntityMonitor<Author>(context: moc, frequency: .OnSave)
+        monitor.setDelegate(self)
+
+        insertExpectation = expectationWithDescription("EntityMonitor Insert Callback")
+        updateExpectation = expectationWithDescription("EntityMonitor Update Callback")
+        deleteExpectation = expectationWithDescription("EntityMonitor Delete Callback")
+
+        // Insert an Item
+        let entity = Author.newInContext(moc)
+        try! moc.saveContextAndWait()
+
+        // New book (since we aren't observing this shouldn't show up in our delegate callback)
+        let _ = Book.newInContext(moc)
+        try! moc.saveContextAndWait()
+
+        // Modify an existing
+        let existing = Author.findFirst(nil, context: moc)!
+        existing.setValue("Robert", forKey: "firstName")
+        moc.saveContext()
+
+        // Delete an item
+        moc.deleteObject(entity)
+        try! moc.saveContextAndWait()
+
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+
+    func testOnChangeNotifications() {
+        // Setup monitor
+        let moc = stack.mainQueueContext
+        monitor = EntityMonitor<Author>(context: moc, frequency: .OnChange)
+        monitor.setDelegate(self)
+
+        // Test Insert
+        insertExpectation = expectationWithDescription("EntityMonitor Insert Callback")
+        let _ = Author.newInContext(moc)
+        waitForExpectationsWithTimeout(10, handler: nil)
+
+        // New Book (since we aren't observing this shouldn't show up in our delegate callback)
+        let _ = Book.newInContext(moc)
+
+        // Test Update
+        updateExpectation = expectationWithDescription("EntityMonitor Update Callback")
+        let existing = Author.findFirst(nil, context: moc)!
+        existing.setValue("Robert", forKey: "firstName")
+        moc.processPendingChanges()
+        waitForExpectationsWithTimeout(10, handler: nil)
+
+        // Test Delete
+        deleteExpectation = expectationWithDescription("EntityMonitor Delete Callback")
+        let deleteMe = Author.newInContext(moc)
+        stack.mainQueueContext.deleteObject(deleteMe)
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+
+    func testFilterPredicate() {
+        // Create filter predicate
+        let matchingLastName = "Edwards"
+        let predicate = NSPredicate(format: "lastName = %@", matchingLastName)
+
+        // Setup monitor
+        let moc = stack.mainQueueContext
+        filteredMonitor = EntityMonitor<Author>(context: moc, filterPredicate: predicate)
+        filteredMonitor.setDelegate(self)
+
+        // Create an initial book
+        let newAuthor = Author.newInContext(moc)
+        try! moc.saveContextAndWait()
+
+        // Look for an update
+        updateExpectation = expectationWithDescription("EntityMonitor Update Callback")
+        newAuthor.lastName = matchingLastName
+        try! moc.saveContextAndWait()
+        waitForExpectationsWithTimeout(10, handler: nil)
+
+        // Look for deletion
+        deleteExpectation = expectationWithDescription("EntityMonitor Delete Callback")
+        moc.deleteObject(newAuthor)
+        try! moc.saveContextAndWait()
+        waitForExpectationsWithTimeout(10, handler: nil)
+    }
+
+    // MARK: - EntityMonitorDelegate
+
+    // Author Monitor
+
+    func entityMonitorObservedDeletions(monitor: EntityMonitor<Author>, entities: Set<Author>) {
+        if monitor === self.monitor {
+            XCTAssertGreaterThan(entities.count, 0)
+            deleteExpectation.fulfill()
+        } else if monitor === filteredMonitor {
+            deleteExpectation.fulfill()
+            XCTAssertEqual(entities.count, 1)
+        }
+    }
+
+    func entityMonitorObservedInserts(monitor: EntityMonitor<Author>, entities: Set<Author>) {
+        if monitor === self.monitor {
+            XCTAssertGreaterThan(entities.count, 0)
+            insertExpectation.fulfill()
+        } else if monitor === self.filteredMonitor {
+            XCTFail("Book inserts will never have a matching title so we shouldn't get this callback")
+        }
+    }
+
+    func entityMonitorObservedModifications(monitor: EntityMonitor<Author>, entities: Set<Author>) {
+        if monitor === self.monitor {
+            XCTAssertGreaterThan(entities.count, 0)
+            XCTAssertNotNil(entities.first?.firstName)
+            updateExpectation.fulfill()
+        } else if monitor === self.filteredMonitor {
+            updateExpectation.fulfill()
+            XCTAssertEqual(entities.count, 1)
+        }
+    }
+}
