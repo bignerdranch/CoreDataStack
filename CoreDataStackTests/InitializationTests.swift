e541e9f9b225c6a62375ded9747f66158e980ea3
davidahouse <davidahouse@gmail.com>
2015-12-18 12:32:01 -0500

    Added tests to cover alternate directory cases

--- CoreDataStackTests/InitializationTests.swift
+++ CoreDataStackTests/InitializationTests.swift
@@ -66,6 +66,75 @@ class CoreDataStackTests: TempDirectoryTestCase {
         
         XCTAssertNotNil(stack.mainQueueContext)
         XCTAssertNotNil(stack.privateQueueContext)
+        XCTAssertTrue(fileExists("TestModel.sqlite", directory: .DocumentDirectory))
+    }
+ 
+    func testInitializationToAlternateDirectory() {
+        let bundle = NSBundle(forClass: CoreDataStackTests.self)
+        let ex1 = expectationWithDescription("SQLite Callback")
+        
+        CoreDataStack.constructSQLiteStack(withModelName: "TestModel", inBundle: bundle, inDirectory: .CachesDirectory) { result in
+            switch result {
+            case .Success(let stack):
+                self.stack = stack
+            case .Failure(let error):
+                print(error)
+                XCTFail()
+            }
+            ex1.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+        
+        XCTAssertNotNil(stack.mainQueueContext)
+        XCTAssertNotNil(stack.privateQueueContext)
+        XCTAssertTrue(fileExists("TestModel.sqlite", directory: .CachesDirectory))
     }
     
+    func testInitializationWithStoreURLOverrideInDirectory() {
+        let bundle = NSBundle(forClass: CoreDataStackTests.self)
+        let ex1 = expectationWithDescription("SQLite Callback")
+        removeIfExists("TestModel.sqlite", directory: .CachesDirectory)
+        CoreDataStack.constructSQLiteStack(withModelName: "TestModel", inBundle: bundle, withStoreURL: tempStoreURL, inDirectory: .CachesDirectory) { result in
+            switch result {
+            case .Success(let stack):
+                self.stack = stack
+            case .Failure(let error):
+                print(error)
+                XCTFail()
+            }
+            ex1.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(10, handler: nil)
+        
+        XCTAssertNotNil(stack.mainQueueContext)
+        XCTAssertNotNil(stack.privateQueueContext)
+        XCTAssertFalse(fileExists("TestModel.sqlite", directory: .CachesDirectory))
+    }
+    
+    private func fileExists(fileName:String,directory:NSSearchPathDirectory) -> Bool {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(directory, inDomains: .UserDomainMask)
+        let fileURL = urls.first?.URLByAppendingPathComponent(fileName)
+        if let filePath = fileURL?.path {
+            return NSFileManager.defaultManager().fileExistsAtPath(filePath)
+        }
+        else {
+            return false
+        }
+    }
+    
+    private func removeIfExists(fileName:String,directory:NSSearchPathDirectory) {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(directory, inDomains: .UserDomainMask)
+        let fileURL = urls.first?.URLByAppendingPathComponent(fileName)
+        if let filePath = fileURL?.path {
+            if NSFileManager.defaultManager().fileExistsAtPath(filePath) {
+                do {
+                    try NSFileManager.defaultManager().removeItemAtPath(filePath)
+                }
+                catch {
+                }
+            }
+        }
+    }
 }
