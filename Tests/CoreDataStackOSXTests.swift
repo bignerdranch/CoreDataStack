f00fd7e69afb4fc448142de537fa2f2e74a9de80
Robert Edwards <robert@bignerdranch.com>
2016-03-10 12:45:04 -0500

    Add an OS X target and unit test target

---
+++ Tests/CoreDataStackOSXTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataStackOSXTests.swift
+//  CoreDataStack
+//
+//  Created by Robert Edwards on 3/1/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import XCTest
+
+@testable import CoreDataStack
+
+class CoreDataStackOSXTests: XCTestCase {
+
+    
+}
