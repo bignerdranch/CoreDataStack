00105ac9602c6865065b46f975cd11bc3adc104b
Dariusz Rybicki <darrarski@gmail.com>
2016-04-08 02:16:12 +0200

    Refactroing

--- Tests/CoreDataModelableTests.swift
+++ Tests/CoreDataModelableTests.swift
@@ -63,7 +63,7 @@ class CoreDataModelableTests: TempDirectoryTestCase {
             
             XCTAssertEqual(updatedBooks.count, 2)
             
-            let updatedBooksTitles = updatedBooks.flatMap({ $0.title })
+            let updatedBooksTitles = updatedBooks.flatMap { $0.title }
             let updatedBooksTitlesExpectation = [
                 "iOS Programming: The Big Nerd Ranch Guide - UPDATED",
                 "Swift Programming: The Big Nerd Ranch Guide - UPDATED"
