0d6a5e2e61bc5b554083b65675a5d8c5489904de
Robert Edwards <robert@bignerdranch.com>
2016-07-14 11:19:14 -0400

    API updates for Swift 2.3

--- Tests/TempDirectoryTestCase.swift
+++ Tests/TempDirectoryTestCase.swift
@@ -16,7 +16,10 @@ class TempDirectoryTestCase: XCTestCase {
 
     private lazy var tempStoreDirectory: NSURL? = {
         let baseURL = NSURL.fileURLWithPath(NSTemporaryDirectory(), isDirectory: true)
-        let tempDir = baseURL.URLByAppendingPathComponent("XXXXXX")
+        guard let tempDir = baseURL.URLByAppendingPathComponent("XXXXXX") else {
+            assertionFailure("Unable to create temp directory URL")
+            return nil
+        }
         do {
             try NSFileManager.defaultManager().createDirectoryAtURL(tempDir,
                 withIntermediateDirectories: true,
