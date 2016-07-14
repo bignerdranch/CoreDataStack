f074f3d9e7fc4823add1e1959ece67a85b6bb107
Robert Edwards <robert@bignerdranch.com>
2016-07-14 13:10:30 -0400

    Add a second sample project to show NSPersistentContainer usage

---
+++ Container Example/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  Container Example
+//
+//  Created by Robert Edwards on 7/14/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    let persistentContainer = NSPersistentContainer(name: "UniqueConstraintModel")
+    private let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    private lazy var loadingVC: UIViewController = {
+        return self.mainStoryboard.instantiateViewControllerWithIdentifier("LoadingVC")
+    }()
+    private lazy var myCoreDataVC: MyCoreDataConnectedViewController = {
+        return self.mainStoryboard.instantiateViewControllerWithIdentifier("CoreDataVC")
+            as! MyCoreDataConnectedViewController
+    }()
+
+    func applicationDidFinishLaunching(application: UIApplication) {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = loadingVC
+
+        persistentContainer.loadPersistentStoresWithCompletionHandler() { storeDescription, error in
+            if let error = error {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            self.seedInitialData()
+            self.myCoreDataVC.persistentContainer = self.persistentContainer
+            self.window?.rootViewController = self.myCoreDataVC
+        }
+
+        window?.makeKeyAndVisible()
+    }
+
+    private func seedInitialData() {
+        let moc = persistentContainer.newBackgroundContext()
+        moc.performBlockAndWait() {
+            if try! Book.countInContext(moc) == 0 {
+                let books = StubbedBookData.books
+                for bookTitle in books {
+                    let book = Book(managedObjectContext: moc)
+                    book.title = bookTitle
+                }
+
+                do {
+                    try moc.save()
+                } catch {
+                    fatalError("Saving records should not fail. Error: \(error)")
+                }
+            }
+        }
+    }
+}
+
