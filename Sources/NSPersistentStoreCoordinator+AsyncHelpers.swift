72d9ddc1b14b668638ed812f846f633fdec26bcd
Robert Edwards <robert@bignerdranch.com>
2017-05-17 10:33:10 -0400

    Replace unsafeBitCast usage with withoutActuallyEscaping
    Additional Context: Starting in Swift 3.1, the optimizer is free to introduce incorrect optimizations for the former implementation.

--- Sources/NSPersistentStoreCoordinator+AsyncHelpers.swift
+++ Sources/NSPersistentStoreCoordinator+AsyncHelpers.swift
@@ -20,29 +20,50 @@ extension NSPersistentStoreCoordinator {
      technically `rethrows`, but cannot be due to Swift limitations.
     **/
     public func performAndWaitOrThrow<Return>(_ body: () throws -> Return) rethrows -> Return {
-        func impl(execute work: () throws -> Return, recover: (Error) throws -> Void) rethrows -> Return {
-            var result: Return!
-            var error: Error?
+        #if swift(>=3.1)
+            return try withoutActuallyEscaping(body) { (work) in
+                var result: Return!
+                var error: Error?
 
-            // performAndWait is marked @escaping as of iOS 10.0.
-            // swiftlint:disable type_name
-            typealias Fn = (() -> Void) -> Void
-            let performAndWaitNoescape = unsafeBitCast(self.performAndWait, to: Fn.self)
-            performAndWaitNoescape {
-                do {
-                    result = try work()
-                } catch let e {
-                    error = e
+                performAndWait {
+                    do {
+                        result = try work()
+                    } catch let e {
+                        error = e
+                    }
+                }
+
+                if let error = error {
+                    throw error
+                } else {
+                    return result
                 }
             }
+        #else
+            func impl(execute work: () throws -> Return, recover: (Error) throws -> Void) rethrows -> Return {
+                var result: Return!
+                var error: Error?
 
-            if let error = error {
-                try recover(error)
+                // performAndWait is marked @escaping as of iOS 10.0.
+                // swiftlint:disable type_name
+                typealias Fn = (() -> Void) -> Void
+                let performAndWaitNoescape = unsafeBitCast(self.performAndWait, to: Fn.self)
+                performAndWaitNoescape {
+                    do {
+                        result = try work()
+                    } catch let e {
+                        error = e
+                    }
+                }
+
+                if let error = error {
+                    try recover(error)
+                }
+
+                return result
             }
 
-            return result
-        }
-
-        return try impl(execute: body, recover: { throw $0 })
+            return try impl(execute: body, recover: { throw $0 })
+        #endif
     }
 }
